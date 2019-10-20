63ebe1cffb6c5636db0ab09d058a6f56ea78d871
hulian425 <52779590+hulian425@users.noreply.github.com>
2019-10-20 14:15:26 +0800

    Create 01 背包问题.cpp

---
+++ 试验5/01 背包问题.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+using namespace std;
+const int N = 7;
+
+int n, w;
+
+int f[N];
+int dir[N];
+
+int calc(int pp[], int ww[])
+{
+	int sum = 0;
+	int W = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		if (f[i])
+		{
+			sum += pp[i];
+			W += ww[i];
+		}
+	}
+	if (W > w) 
+		return -1;
+	return sum;
+}
+
+int solve(int pp[], int ww[])
+{
+	int ans = 0;
+	for (int i = 0; i < 1 << N; i++)
+	{
+		memset(f, 0, sizeof(f));
+		for (int j = 0; j < N; j++)
+		{
+			f[N - j - 1] = i >> j & 1;
+		}
+		
+		int num = calc(pp, ww);
+		ans = max(ans, num);
+	}
+	return ans;
+}
+
+
+int main()
+{
+	n = 7;
+	w = 15;
+	int p[7] = { 10, 5, 15, 7, 6, 18, 3 };
+	int w[7] = { 2, 3, 5, 7, 1, 4, 1 };
+	cout << "the ans is "<< solve(p, w) << endl;
+
+} 
